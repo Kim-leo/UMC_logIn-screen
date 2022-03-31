e130c4ab70e50cf8b9ebd4a6d2b2cb2a0eac01d7
Kim - leo <ochell2@naver.com>
2022-03-31 14:21:12 +0900

    Initial Commit

---
+++ UMC_logIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_logIn
+//
+//  Created by 김승현 on 2022/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
