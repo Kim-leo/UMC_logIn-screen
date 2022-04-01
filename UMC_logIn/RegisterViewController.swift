5a7a2638acba23c367123ee98f021f471d18a4e4
Kim - leo <ochell2@naver.com>
2022-04-01 15:46:47 +0900

    회원가입 액션연결

--- UMC_logIn/RegisterViewController.swift
+++ UMC_logIn/RegisterViewController.swift
@@ -8,14 +8,65 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    // MARK: - Properties
+    // 유효성검사를 위한 프로퍼티
+    var isValidEmail = false
+    
+    var isValidName = false
+    
+    var isValidNickname = false
+    
+    var isValidPassword = false
+    
+    // TextField
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nicknameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var textField: [UITextField] {
+        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
+    }
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTextField()
         
+    }
+    // MARK: - Actions
+    @objc
+    func textFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        
+        switch sender {
+        case emailTextField:
+            print("email")
+        case nameTextField:
+            print("name")
+        case nicknameTextField:
+            print("nickname")
+        case passwordTextField:
+            print("password")
+        default:
+            fatalError("Missing TextField...")
+        }
     }
     
 
     
-
+    
+    // MARK: - Helpers
+    private func setupTextField() {
+        
+        textField.forEach { tf in
+            tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        }
+        
+        
+    }
 }
